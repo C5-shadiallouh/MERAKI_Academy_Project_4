8458dd0b3ba3eda7fe0b7057ea683fb5741a4089
shadiallouh <sh.m.allouh@gmail.com>
2022-05-09 22:02:06 +0300

    build userschema

---
+++ backend/models/users.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  phone: { type: String, required: true, unique: true },
+  city: { type: String },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  role: {type: mongoose.Schema.Types.ObjectId ,ref:"Roles"},
+});
+userSchema.pre("save", async function () {
+  this.email = this.email.toLowerCase();
+  this.password = await bcrypt.hash(this.password, process.env.SALT);
+});
+module.exports = mongoose.model("User",userSchema)
